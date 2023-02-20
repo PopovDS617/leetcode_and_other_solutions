8212c32eb13b90062cb0458ce97d0c1d4dae4e8c
PopovDS617 <popovds.bm@gmail.com>
2023-02-20 19:28:16 +0300

    new solutions

--- hackerrank/find-lonely-integer.ts
+++ hackerrank/find-lonely-integer.ts
@@ -1,15 +1,44 @@
 //find a lonely integer in array of duplicates
 
-let duplicates:any =[]
-  duplicates=a.filter((num,index)=>{
-    return a.indexOf(num)!==index
-  })
-  
-  const result:number[]|number=a.filter(item=>{
-    if(!duplicates.includes(item)){return item}
-      
-  })
-  const final:number=result[0]
-  
-   return final
-}
+// var 1
+const foo = (array) => {
+  let duplicates: any = [];
+  duplicates = a.filter((num, index) => {
+    return a.indexOf(num) !== index;
+  });
+
+  const result: number[] | number = a.filter((item) => {
+    if (!duplicates.includes(item)) {
+      return item;
+    }
+  });
+  const final: number = result[0];
+
+  return final;
+};
+
+// var 2
+const foo = (array) => {
+  const store = {};
+
+  for (let number in array) {
+    const testNumber = array[number];
+
+    if (store[testNumber]) {
+      store[testNumber]++;
+    } else {
+      store[testNumber] = 1;
+    }
+  }
+
+  const result = [];
+
+  for (let [key, value] of Object.entries(store)) {
+    console.log(key, value);
+    if (value < 2) {
+      result.push(+key);
+    }
+  }
+
+  return result;
+};
