3d6502f0f72335bbabaaa804953dbd867e2dd19e
Dmitry Popov <109096089+PopovDS617@users.noreply.github.com>
2023-03-17 17:33:06 +0300

    Update recreate-promise-all.ts

--- other/recreate-promise-all.ts
+++ other/recreate-promise-all.ts
@@ -1,18 +1,32 @@
   const customPromiseAll = (array) => {
-    const results = [];
-    let count = 0;
-    return new Promise((resolve, rej) => {
-      for (let i = 0; i < array.length; i++) {
-        array[i]
-          .then((res) => {
-            results[i] = res;
-            count++;
+      const results = [];
+      let count = 0;
+      return new Promise((resolve, rej) => {
+        for (let i = 0; i < array.length; i++) {
+          array[i]
+            .then((res) => {
+              results[i] = res;
+              count++;
 
-            if (count === array.length) {
-              resolve(results);
-            }
-          })
-          .catch((err) => rej(err));
-      }
+              if (count === array.length) {
+                resolve(results);
+              }
+            })
+            .catch((err) => rej(err));
+        }
+      });
+    };
+
+    const p1 = new Promise((res) => {
+      setTimeout(() => {
+        res(1);
+      }, 2500);
     });
-  };
+    const p2 = new Promise((res) => res(11));
+
+    const fetchAll = async (arr) => {
+      const result = await customPromiseAll(arr);
+
+      setState(result);
+    };
+    fetchAll([p1, p2]);
